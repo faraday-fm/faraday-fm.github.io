cb2172155adcd9a50005ef5297b27247d9dd37a1
Mikhail Isupov <lam0x86@gmail.com>
2023-03-12 16:47:15 +0300

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  if (command === 'build') {
+  if (command === 'serve') {
     return {
       plugins: [react()],
       optimizeDeps: { exclude: ["@far-more/web-ui"] },
