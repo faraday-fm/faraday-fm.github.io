8a0f5b3bb2b33142c4b71b19365bae416a874278
Mikhail Isupov <lam0x86@gmail.com>
2023-03-20 08:41:39 +0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,46 @@ import { defineConfig, Plugin, UserConfig } from "vite";
 import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { zipFolderPlugin } from "./zipFolderPlugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const localLibs = process.env.LOCAL_LIBS === "true";
 
-  const plugins = [react(), zipFolderPlugin()];
+  const plugins = [
+    react(),
+    zipFolderPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "inline",
+      // workbox: {
+      //   globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      // },
+      includeAssets: ["faraday.svg", "faraday_180.png", "faraday-mask.svg"],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Faraday",
+        short_name: "Faraday",
+        description: "Faraday file manager",
+        theme_color: "#1e1e1e",
+        icons: [
+          {
+            src: "faraday_512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "faraday_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ];
 
   let config: UserConfig = {
     plugins,
