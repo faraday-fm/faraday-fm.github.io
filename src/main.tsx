9f6b85fe10b92b9eccd55f41360c468fd572635c
Mikhail Isupov <lam0x86@gmail.com>
2023-03-20 09:14:48 +0300

    pwa

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// replaced dyanmicaly
+const date = "__DATE__";
+
+console.info("Built at: " + date);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,76 @@
-import { defineConfig, Plugin, UserConfig } from "vite";
+import { defineConfig, Plugin, PluginOption, UserConfig } from "vite";
 import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { zipFolderPlugin } from "./zipFolderPlugin";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, VitePWAOptions, ManifestOptions } from "vite-plugin-pwa";
+import replace from "@rollup/plugin-replace";
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  injectRegister: "inline",
+  workbox: {
+    globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+    maximumFileSizeToCacheInBytes: 10000000,
+  },
+  includeAssets: ["faraday.svg", "faraday_180.png", "faraday-mask.svg"],
+  devOptions: {
+    enabled: true,
+  },
+  minify: true,
+  manifest: {
+    name: "Faraday",
+    short_name: "Faraday",
+    description: "Faraday file manager",
+    theme_color: "#1e1e1e",
+    background_color: "#1e1e1e",
+    icons: [
+      {
+        src: "faraday_512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "faraday_512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const localLibs = process.env.LOCAL_LIBS === "true";
 
-  const plugins = [
+  const replaceOptions = { __DATE__: new Date().toISOString() };
+  const claims = process.env.CLAIMS === "true";
+  const reload = process.env.RELOAD_SW === "true";
+  const selfDestroying = process.env.SW_DESTROY === "true";
+
+  if (process.env.SW === "true") {
+    pwaOptions.srcDir = "src";
+    pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
+    pwaOptions.strategies = "injectManifest";
+    (pwaOptions.manifest as Partial<ManifestOptions>).name =
+      "PWA Inject Manifest";
+    (pwaOptions.manifest as Partial<ManifestOptions>).short_name = "PWA Inject";
+  }
+
+  if (claims) pwaOptions.registerType = "autoUpdate";
+
+  if (reload) {
+    // @ts-expect-error just ignore
+    replaceOptions.__RELOAD_SW__ = "true";
+  }
+
+  if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
+
+  const plugins: PluginOption[] = [
     react(),
     zipFolderPlugin(),
-    VitePWA({
-      registerType: "prompt",
-      injectRegister: "inline",
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-        maximumFileSizeToCacheInBytes: 10000000,
-      },
-      includeAssets: ["faraday.svg", "faraday_180.png", "faraday-mask.svg"],
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "Faraday",
-        short_name: "Faraday",
-        description: "Faraday file manager",
-        theme_color: "#1e1e1e",
-        background_color: "#1e1e1e",
-        icons: [
-          {
-            src: "faraday_512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "faraday_512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
+    replace(replaceOptions) as any,
+    VitePWA(pwaOptions),
   ];
 
   let config: UserConfig = {
