ad31291a296bcb44c1e719441092bbe70e67cf15
Mikhail Isupov <lam0x86@gmail.com>
2024-08-05 22:53:11 +0300

    sftp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { Faraday, FaradayHost } from "@frdy/web-ui";
-import { buildDemoFs, buildFaradayFs } from "./services/fs";
-import { GitHubFs } from "./services/GitHubFs";
-import { WebFs } from "./services/WebFs";
+import { buildFaradayFs } from "./services/fs";
 
 const faradayFs = await buildFaradayFs();
-// const demoFs = await buildDemoFs();
-// demoFs.createDirectory("mount");
-// demoFs.mount("github", new GitHubFs("faraday-fm", "web-ui"));
-
-// faradayFs.mount('demo', demoFs);
 
 const host: FaradayHost = {
   config: {
@@ -18,28 +11,9 @@ const host: FaradayHost = {
   rootFs: faradayFs,
 };
 
-const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-  e.preventDefault();
-  e.stopPropagation();
-};
-
-const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-  const root = e.dataTransfer.items[0].webkitGetAsEntry();
-  if (root && root.isDirectory) {
-    e.preventDefault();
-    e.stopPropagation();
-    // demoFs.mount(
-    //   "mount/" + root.name,
-    //   new WebFs(root as FileSystemDirectoryEntry)
-    // );
-  }
-};
-
 function App() {
   return (
     <div
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}
       style={{ width: "100%", height: "100%", display: "grid" }}
     >
       <Faraday host={host} />
